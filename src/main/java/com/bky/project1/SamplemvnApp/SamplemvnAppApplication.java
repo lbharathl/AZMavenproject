10147366dbae05407ef3483ff9a4da15a10f09fc
lbharathl <bharathboini3@gmail.com>
2021-11-27 17:06:53 +0530

    Maven Project

---
+++ src/main/java/com/bky/project1/SamplemvnApp/SamplemvnAppApplication.java
@@ -0,0 +1,13 @@
+package com.bky.project1.SamplemvnApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplemvnAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplemvnAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bky/project1/SamplemvnApp/SamplemvnAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bky.project1.SamplemvnApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplemvnAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
